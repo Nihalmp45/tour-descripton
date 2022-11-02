aecf437bd70e33b2db5c0997069f40ff35c503a0
Nihal Abdul Khader MP <nihalmp45@gmail.com>
2022-11-02 20:12:25 +0530

    initial

---
+++ setup/src/Tour.js
@@ -0,0 +1,18 @@
+import React, { useState } from 'react';
+
+const Tour = ({id,image,info,price,name,removeTour}) => {
+  return (
+    <article className='single-tour'>
+      <img src={image} alt={name}></img>
+      <footer>
+        <div className='tour-info'>
+          <h4>{name}</h4>
+          <h4 className='tour-price'>${price}</h4>
+        </div>
+          <p>{info}</p>
+          <button className='delete-btn' onClick={()=>removeTour(id)}>not interested</button>
+      </footer>
+    </article>
+)};
+
+export default Tour;
